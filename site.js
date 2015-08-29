45f72455137815cc54f74ed9091d62a6190493df
Martin Dybal <martin@dybal.it>
2015-08-29 13:46:50 +0200

    Přepínání mezi detailem a seznamem výrobků

---
+++ site.js
@@ -0,0 +1,22 @@
+﻿window.onpopstate = function () {
+    $('#vyrobky').toggle();
+    $('#vyrobek-detail').toggle();
+};
+
+$(".vyrobek").click(function () {
+    var vyrobek = $(this);
+    var detailVyrobku = $('#vyrobek-detail');
+    var vyrobky = $('#vyrobky');
+
+    var nadpisVyrobku = vyrobek.find('.title').text();
+    detailVyrobku.find('.title').text(nadpisVyrobku);
+    detailVyrobku.find('.image').attr("src", vyrobek.find('.image').attr("src"));
+    detailVyrobku.find('.descrition').text(vyrobek.find('.descrition').text());
+
+    vyrobky.toggle();
+    detailVyrobku.toggle();
+
+    var title = 'Detail výrobku: ' + nadpisVyrobku;
+    document.title = title;
+    window.history.pushState("", title, '/detail');
+});
