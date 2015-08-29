0edfdd128dfaf2bbf62223eb515fe9921f1a56f2
Martin Dybal <martin@dybal.it>
2015-08-29 16:02:07 +0200

    Nastylování detailu

--- site.js
+++ site.js
@@ -1,6 +1,9 @@
 ﻿window.onpopstate = function () {
-    $('#vyrobky').toggle();
-    $('#vyrobek-detail').toggle();
+    //$('#vyrobky').toggle();
+    //$('#vyrobek-detail').toggle();
+    if (location.search !== '?detail') {
+        location.reload();
+    }
 };
 
 $(".vyrobek").click(function () {
@@ -10,7 +13,24 @@ $(".vyrobek").click(function () {
 
     var nadpisVyrobku = vyrobek.find('.title').text();
     detailVyrobku.find('.title').text(nadpisVyrobku);
-    detailVyrobku.find('.image').attr("src", vyrobek.find('.image').attr("src"));
+
+    detailVyrobku.find('.main-image').attr("href", vyrobek.find('.main-image').attr("src"));
+    detailVyrobku.find('.main-image img').attr("src", vyrobek.find('.main-image').attr("src"));
+
+    detailVyrobku.find('.image-1').attr("href", vyrobek.find('.image-1').attr("src"));
+    detailVyrobku.find('.image-1 img').attr("src", vyrobek.find('.image-1').attr("src"));
+    
+    detailVyrobku.find('.image-2').attr("href", vyrobek.find('.image-2').attr("src"));
+    detailVyrobku.find('.image-2 img').attr("src", vyrobek.find('.image-2').attr("src"));
+    
+    detailVyrobku.find('.image-3').attr("href", vyrobek.find('.image-3').attr("src"));
+    detailVyrobku.find('.image-3 img').attr("src", vyrobek.find('.image-3').attr("src"));
+    
+    detailVyrobku.find('.image-4').attr("href", vyrobek.find('.image-4').attr("src"));
+    detailVyrobku.find('.image-4 img').attr("src", vyrobek.find('.image-4').attr("src"));
+    
+
+
     detailVyrobku.find('.descrition').text(vyrobek.find('.descrition').text());
 
     vyrobky.toggle();
@@ -18,5 +38,5 @@ $(".vyrobek").click(function () {
 
     var title = 'Detail výrobku: ' + nadpisVyrobku;
     document.title = title;
-    window.history.pushState("", title, '/detail');
+    window.history.pushState("", title, '?detail');
 });
