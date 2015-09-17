4db5157a05b755f122872289b8fb97360b499d49
Martin Dybal <martin@dybal.it>
2015-09-17 11:34:30 +0200

    Předěláno do SPA Angular

---
+++ app.js
@@ -0,0 +1,96 @@
+﻿(function () {
+    var app = angular.module('TotebagApp', []);
+
+    app.controller('TotebagController', function () {
+        var self = this;
+        self.selectedItem = {};
+        self.items = bags;
+
+        self.hideAllPages = function () {
+            $("#vyrobky").hide();
+            $("#vyrobek-detail").hide();
+            $("#o-nas").hide();
+            $("#nas-pribeh").hide();
+            $("#kontakty").hide();
+
+        }
+
+        self.GoToDetail = function (item) {
+            self.selectedItem = item;
+            self.hideAllPages();
+            $("#vyrobek-detail").fadeIn();
+
+            var title = 'Detail výrobku: ' + item.title;
+            document.title = title;
+            window.history.pushState("", title, '?detail');
+        }                                        
+                                                 
+        self.GoToBags = function () {
+            self.items = bags;
+            self.hideAllPages();
+            $("#vyrobky").fadeIn();              
+                                                 
+            var title = 'Totebag - tašky';       
+            document.title = title;              
+            window.history.pushState("", title, '?tašky');
+        }                                        
+                                                 
+        self.GoToPillows = function () {
+            self.items = pillows;
+            self.hideAllPages();
+            $("#vyrobky").fadeIn();              
+                                                 
+            var title = 'Totebag - polštáře';    
+            document.title = title;              
+            window.history.pushState("", title, '?polštáře');
+        }                                        
+                                                 
+        self.GoToDecorations = function () {
+            self.items = decorations;
+            self.hideAllPages();
+            $("#vyrobky").fadeIn();              
+                                                 
+            var title = 'Totebag - dekorace';    
+            document.title = title;              
+            window.history.pushState("", title, '?dekorace');
+        }                                        
+                                                 
+        self.GoToAboutUs = function () {
+            self.items = decorations;
+            self.hideAllPages();
+            $("#o-nas").fadeIn();                
+                                                 
+            var title = 'Totebag - o nás';    
+            document.title = title;              
+            window.history.pushState("", title, '?o-nás');
+        }
+
+        self.GoToOurStory = function () {
+            self.items = decorations;
+            self.hideAllPages();
+            $("#nas-pribeh").fadeIn();
+                                                 
+            var title = 'Totebag - náš příběh';
+            document.title = title;              
+            window.history.pushState("", title, '?náš-příběh');
+        }
+
+        self.GoToContacts = function () {
+            self.items = decorations;
+            self.hideAllPages();
+            $("#kontakty").fadeIn();
+                                                 
+            var title = 'Totebag - kontakty';
+            document.title = title;              
+            window.history.pushState("", title, '?kontakty');
+        }
+
+        window.onpopstate = function () {
+            if (location.search !== '?detail') {
+                self.GoToDecorations();
+            }
+        };
+    });
+
+    $("#vyrobky").show();
+})();
---
+++ products.js
@@ -0,0 +1,71 @@
+﻿var bags = [
+    {
+        title: "Košíček s látkovými květy",
+        description: "Proutěný košíček plný látkových květů, s bavlněnou krajkou. Květy jsou ručně šité, středy jsou dozdobeny různými aplikacemi. Organtinová mašlička, imitace štrasu, síťová krajka, mašličky, krajka. Výška cca 18cm, průměr cca 25cm.",
+        mainImage: { src: "http://img.flercdn.net/i2/products/8/1/3/77318/6/2/6299483/iydfamovcqvaqu.jpg", },
+        image1: { src: "http://img.flercdn.net/i2/products/8/1/3/77318/6/2/6299483/iydfamovcqvaqu.jpg", },
+        image2: { src: "http://img.flercdn.net/i2/products/8/1/3/77318/6/2/6299483/iydfamovcqvaqu.jpg", },
+        image3: { src: "http://img.flercdn.net/i2/products/8/1/3/77318/6/2/6299483/iydfamovcqvaqu.jpg", },
+        image4: { src: "http://img.flercdn.net/i2/products/8/1/3/77318/6/2/6299483/iydfamovcqvaqu.jpg", },
+    },
+    {
+        title: "Košíček s látkovými květy",
+        description: "Proutěný košíček plný látkových květů, s bavlněnou krajkou. Květy jsou ručně šité, středy jsou dozdobeny různými aplikacemi. Organtinová mašlička, imitace štrasu, síťová krajka, mašličky, krajka. Výška cca 18cm, průměr cca 25cm.",
+        mainImage: { src: "http://img.flercdn.net/products/7/7/77318/3/2/9/6586923/qtycozzorjysrt-b.jpg", },
+        image1: { src: "http://img.flercdn.net/products/7/7/77318/3/2/9/6586923/qtycozzorjysrt-b.jpg", },
+        image2: { src: "http://img.flercdn.net/products/7/7/77318/3/2/9/6586923/qtycozzorjysrt-b.jpg", },
+        image3: { src: "http://img.flercdn.net/products/7/7/77318/3/2/9/6586923/qtycozzorjysrt-b.jpg", },
+        image4: { src: "http://img.flercdn.net/products/7/7/77318/3/2/9/6586923/qtycozzorjysrt-b.jpg", },
+    },
+    {
+        title: "Košíček s látkovými květy",
+        description: "Proutěný košíček plný látkových květů, s bavlněnou krajkou. Květy jsou ručně šité, středy jsou dozdobeny různými aplikacemi. Organtinová mašlička, imitace štrasu, síťová krajka, mašličky, krajka. Výška cca 18cm, průměr cca 25cm.",
+        mainImage: { src: "http://img.flercdn.net/i2/products/8/1/3/77318/6/2/6299483/iydfamovcqvaqu.jpg", },
+        image1: { src: "http://img.flercdn.net/i2/products/8/1/3/77318/6/2/6299483/iydfamovcqvaqu.jpg", },
+        image2: { src: "http://img.flercdn.net/i2/products/8/1/3/77318/6/2/6299483/iydfamovcqvaqu.jpg", },
+        image3: { src: "http://img.flercdn.net/i2/products/8/1/3/77318/6/2/6299483/iydfamovcqvaqu.jpg", },
+        image4: { src: "http://img.flercdn.net/i2/products/8/1/3/77318/6/2/6299483/iydfamovcqvaqu.jpg", },
+    }
+];
+
+var pillows = [
+    {
+        title: "Košíček s látkovými květy",
+        description: "Proutěný košíček plný látkových květů, s bavlněnou krajkou. Květy jsou ručně šité, středy jsou dozdobeny různými aplikacemi. Organtinová mašlička, imitace štrasu, síťová krajka, mašličky, krajka. Výška cca 18cm, průměr cca 25cm.",
+        mainImage: { src: "http://img.flercdn.net/i2/products/8/1/3/77318/6/2/6299483/iydfamovcqvaqu.jpg", },
+        image1: { src: "http://img.flercdn.net/i2/products/8/1/3/77318/6/2/6299483/iydfamovcqvaqu.jpg", },
+        image2: { src: "http://img.flercdn.net/i2/products/8/1/3/77318/6/2/6299483/iydfamovcqvaqu.jpg", },
+        image3: { src: "http://img.flercdn.net/i2/products/8/1/3/77318/6/2/6299483/iydfamovcqvaqu.jpg", },
+        image4: { src: "http://img.flercdn.net/i2/products/8/1/3/77318/6/2/6299483/iydfamovcqvaqu.jpg", },
+    },
+    {
+        title: "Košíček s látkovými květy",
+        description: "Proutěný košíček plný látkových květů, s bavlněnou krajkou. Květy jsou ručně šité, středy jsou dozdobeny různými aplikacemi. Organtinová mašlička, imitace štrasu, síťová krajka, mašličky, krajka. Výška cca 18cm, průměr cca 25cm.",
+        mainImage: { src: "http://img.flercdn.net/products/7/7/77318/3/2/9/6586923/qtycozzorjysrt-b.jpg", },
+        image1: { src: "http://img.flercdn.net/products/7/7/77318/3/2/9/6586923/qtycozzorjysrt-b.jpg", },
+        image2: { src: "http://img.flercdn.net/products/7/7/77318/3/2/9/6586923/qtycozzorjysrt-b.jpg", },
+        image3: { src: "http://img.flercdn.net/products/7/7/77318/3/2/9/6586923/qtycozzorjysrt-b.jpg", },
+        image4: { src: "http://img.flercdn.net/products/7/7/77318/3/2/9/6586923/qtycozzorjysrt-b.jpg", },
+    }
+];
+
+var decorations = [
+    {
+        title: "Košíček s látkovými květy",
+        description: "Proutěný košíček plný látkových květů, s bavlněnou krajkou. Květy jsou ručně šité, středy jsou dozdobeny různými aplikacemi. Organtinová mašlička, imitace štrasu, síťová krajka, mašličky, krajka. Výška cca 18cm, průměr cca 25cm.",
+        mainImage: { src: "http://img.flercdn.net/products/7/7/77318/3/2/9/6586923/qtycozzorjysrt-b.jpg", },
+        image1: { src: "http://img.flercdn.net/products/7/7/77318/3/2/9/6586923/qtycozzorjysrt-b.jpg", },
+        image2: { src: "http://img.flercdn.net/products/7/7/77318/3/2/9/6586923/qtycozzorjysrt-b.jpg", },
+        image3: { src: "http://img.flercdn.net/products/7/7/77318/3/2/9/6586923/qtycozzorjysrt-b.jpg", },
+        image4: { src: "http://img.flercdn.net/products/7/7/77318/3/2/9/6586923/qtycozzorjysrt-b.jpg", },
+    },
+    {
+        title: "Košíček s látkovými květy",
+        description: "Proutěný košíček plný látkových květů, s bavlněnou krajkou. Květy jsou ručně šité, středy jsou dozdobeny různými aplikacemi. Organtinová mašlička, imitace štrasu, síťová krajka, mašličky, krajka. Výška cca 18cm, průměr cca 25cm.",
+        mainImage: { src: "http://img.flercdn.net/i2/products/8/1/3/77318/6/2/6299483/iydfamovcqvaqu.jpg", },
+        image1: { src: "http://img.flercdn.net/i2/products/8/1/3/77318/6/2/6299483/iydfamovcqvaqu.jpg", },
+        image2: { src: "http://img.flercdn.net/i2/products/8/1/3/77318/6/2/6299483/iydfamovcqvaqu.jpg", },
+        image3: { src: "http://img.flercdn.net/i2/products/8/1/3/77318/6/2/6299483/iydfamovcqvaqu.jpg", },
+        image4: { src: "http://img.flercdn.net/i2/products/8/1/3/77318/6/2/6299483/iydfamovcqvaqu.jpg", },
+    },
+];
